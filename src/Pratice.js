a8d3e6ea0978eb0f4b6eff59e564d6fd5a446c63
srikanthsiddatam <siddatamsrikanth7@gmail.com>
2024-01-08 11:57:05 +0530

    these is usersgmt

---
+++ src/Pratice.js
@@ -0,0 +1,7 @@
+const Pratice=()=>{
+    return <>
+    <h1>i am Pratice in html</h1>
+    </>
+
+}
+export default Pratice
---
+++ src/Users.js
@@ -0,0 +1,15 @@
+const Users=(props)=>{
+    const{name,name1,name2}=props
+  return  <>
+  <h1>
+    {name}
+  </h1>
+   <h1>
+    {name1}
+   </h1>
+   <h1>
+    {name2}
+   </h1>
+    </>
+}
+export default Users
